121fa52d94a5cc3db0e42a7c2d36c5862a56fd1b
Chelsi Jain <69745609+Chelsi-create@users.noreply.github.com>
2025-06-05 00:20:03 -0700

    Update preprocess_summaries.sh

--- scripts/preprocess_summaries.sh
+++ scripts/preprocess_summaries.sh
@@ -6,7 +6,7 @@ python preprocess/generate_summaries.py \
     --input_file data/MMLongBench/samples.json \
     --data_base_path data/MMLongBench/documents \
     --output_dir outputs/qwen25_32b/summaries \
-    --model Qwen/Qwen2.5-VL-7B-Instruct \
+    --model Qwen/Qwen2.5-VL-32B-Instruct \
     --api_key_file ./deepinfrakey \
     --base_url http://dgxh-2.hpc.engr.oregonstate.edu:8090/v1 \
     --prompt_file prompts/general_summary_prompt.txt \
